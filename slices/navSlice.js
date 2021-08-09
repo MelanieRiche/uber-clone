23aaea2fabc5c35ec1bc0a9ab0f5e153c88360bc
Melanie <contact.mriche@gmail.com>
2021-08-09 22:26:44 +0200

    Set up Redux - Provider + selectors + HomeScreen component

---
+++ slices/navSlice.js
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  origin: null,
+  destination: null,
+  travelTimeInformation: null,
+};
+
+export const navSlice = createSlice({
+  name: "nav",
+  initialState,
+  reducers: {
+    // 1. dispatch an action 2.pass through a value 3.value will come through (payload) 4. and update the state
+    setOrigin: (state, action) => {
+      state.origin = action.payload;
+    },
+    setDestination: (state, action) => {
+      state.destination = action.payload;
+    },
+    setTravelTimeInformation: (state, action) => {
+      state.travelTimeInformation = action.payload;
+    },
+  },
+});
+
+// Action creators are generated for each case reducer function / Destructuring
+export const { setOrigin, setDestination, setTravelTimeInformation } =
+  navSlice.actions;
+
+// Selectors
+// When I use the selector, it goes to the state and return the current value
+export const selectOrigin = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destination;
+export const selectTravelTimeInformation = (state) =>
+  state.nav.travelTimeInformation;
+
+export default navSlice.reducer;
