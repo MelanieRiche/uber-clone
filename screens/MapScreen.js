60a69bb80b4ce1af339bd62da6b230e4b5501095
Melanie <contact.mriche@gmail.com>
2021-08-25 19:26:11 +0200

    Add a button to to go back to HomeScreen

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,15 +1,23 @@
+import { useNavigation } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import Map from "../components/Map";
 import NavigateCard from "../components/NavigateCard";
 import RideOptionsCard from "../components/RideOptionsCard";
+import { Icon } from "react-native-elements/dist/icons/Icon";
 
 const MapScreen = () => {
   const Stack = createStackNavigator();
   return (
     <View>
+      <TouchableOpacity
+        onPress={() => navigation.navigate("HomeScreen")}
+        style={tw`absolute z-50 top-16 left-5 bg-gray-100 rounded-full shadow-lg p-3`}
+      >
+        <Icon name="menu" />
+      </TouchableOpacity>
       <View style={tw`h-1/2`}>
         <Map />
       </View>
